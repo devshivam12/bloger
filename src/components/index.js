44861c2ca17f2b626df2a313f1f0c033e6842d8f
Shivam <mittalshivam440@gmail.com>
2024-02-22 08:15:32 +0530

    initialization

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./header/Header";
+import Select from '../components/Select'
+import Footer from './footer/Footer'
+import Container from './container/Container'
+import Logo from './Logo'
+import LogOutBtn from "./header/LogOutBtn";
+import RTE from "./RTE";
+import Login from './Login'
+import SignUp from './SignUp'
+import Input from '../components/Input'
+import Button from '../components/Button'
+import PostForm from '../components/post-form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    Input,
+    Button,
+    Select,
+    LogOutBtn,
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
+}
---
+++ src/pages/Loigin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Login as Logincomponent } from '../components'
+const Loigin = () => {
+  return (
+    <div className='py-8'>
+        <Logincomponent />
+    </div>
+  )
+}
+
+export default Loigin
